a52b5c6bfd0a7d410e1a7a680d162d635a88d017
Alan Ricardo Ladino Tunarrosa <aladino@grupocinte.com>
2026-07-07 16:04:52 -0500

    fix: allow pinggy header in cors

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-cinte-xsrf', 'X-Request-Id'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-cinte-xsrf', 'X-Request-Id', 'Bypass-Tunnel-Reminder', 'X-Pinggy-No-Screen'],
     exposedHeaders: ['X-Request-Id']
 }));
 
