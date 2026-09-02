dbb629a6d9b5360026f6b943493a606631634a2b
Luis Miguel Correa <lcorrea@grupocinte.com>
2026-09-02 14:08:13 -0500

    feat(ch): AUT-337 pestañas y campos de ficha Consultores
    General, Datos del puesto, Financiera y Complementario segun la sesion CH. Cabecera con gerente CINTE y comercial; lider del cliente en el puesto.

--- react-frontend/src/onboarding/fichaCatalogos.js
+++ react-frontend/src/onboarding/fichaCatalogos.js
@@ -1,6 +1,33 @@
 /** Catálogos cerrados y edad de la ficha CH (AUT-315). */
 
-export const FICHA_HIDDEN_KEYS = ['estado_catalogo', 'segundo_idioma', 'modalidad_adicional'];
+export const FICHA_HIDDEN_KEYS = [
+    'estado_catalogo',
+    'segundo_idioma',
+    'modalidad_adicional',
+    'controller_staff',
+    'email_gerente_servicio',
+    'ejecucion_horario_no_habil',
+    'direccion_proyecto',
+    'politica_viaticos',
+    'seguimiento_pp',
+    'desempeno_ed_servicio',
+    'dia_familia',
+    'ficha_extension_proyecto',
+    'contacto_focal_1_nombre',
+    'contacto_focal_1_cargo',
+    'contacto_focal_1_movil',
+    'contacto_focal_1_email',
+    'contacto_focal_2_nombre',
+    'contacto_focal_2_cargo',
+    'contacto_focal_2_movil',
+    'contacto_focal_2_email',
+    'contacto_admin_nombre',
+    'contacto_admin_cargo',
+    'contacto_admin_movil',
+    'contacto_admin_email',
+    'primer_contacto_familiar',
+    'segundo_contacto_familiar'
+];
 
 export const FICHA_SECCION_INDICADORES = 'Indicadores y costos';
 
@@ -24,6 +51,14 @@ export const TIPO_CONTRATO_OPTIONS = ['Indefinido', 'Obra o labor', 'Fijo'];
 export const MODALIDAD_CONTRATO_OPTIONS = ['OPS', 'Ordinario', 'Integral'];
 
 export const MODALIDAD_TRABAJO_OPTIONS = ['Híbrido', 'Remoto', 'Presencial'];
+
+export const REALIZADO_PENDIENTE_OPTIONS = ['Realizado', 'Pendiente'];
+
+export const SI_NO_OPTIONS = ['Sí', 'No'];
+
+export const FORMA_PAGO_OPTIONS = ['Mes', 'Hora'];
+
+export const MONEDA_FICHA_OPTIONS = ['COP', 'USD'];
 
 export const DEPARTAMENTO_OPTIONS = [
     'Amazonas',
@@ -69,7 +104,15 @@ export const FICHA_SELECT_OPTIONS = {
     esquema_contrato: ESQUEMA_CONTRATO_OPTIONS,
     tipo_contrato: TIPO_CONTRATO_OPTIONS,
     modalidad_contrato: MODALIDAD_CONTRATO_OPTIONS,
-    modalidad_trabajo: MODALIDAD_TRABAJO_OPTIONS
+    modalidad_trabajo: MODALIDAD_TRABAJO_OPTIONS,
+    periodicidad_pago: FORMA_PAGO_OPTIONS,
+    moneda: MONEDA_FICHA_OPTIONS,
+    teletrabajo: SI_NO_OPTIONS,
+    induccion: REALIZADO_PENDIENTE_OPTIONS,
+    reinduccion: REALIZADO_PENDIENTE_OPTIONS,
+    iso_9001_contextualizacion: REALIZADO_PENDIENTE_OPTIONS,
+    sgsti_descripcion: REALIZADO_PENDIENTE_OPTIONS,
+    iso_14001_ambiental: REALIZADO_PENDIENTE_OPTIONS
 };
 
 /** Años cumplidos hasta `hoy` (civil). Vacío si la fecha no sirve. */
